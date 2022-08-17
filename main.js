8bf890fd80cd35c1ad0e5df5b4067e295c9e3100
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-08-17 11:30:51 +0200

    dev: retrieve and insert element inside doc

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 console.log("start");
+
+var samples = 10;
+console.log("samples: ", samples);
+
+for (var i = 0; i < samples; i++) {
+  console.log("i: ", i);
+
+  var svgns = "http://www.w3.org/2000/svg";
+  var container = document.querySelector("g.samples");
+
+  var x = 34;
+  var y = 56;
+
+  var circle = document.createElementNS(svgns, "circle");
+  circle.setAttributeNS(null, "cx", x);
+  circle.setAttributeNS(null, "cy", y);
+  circle.setAttributeNS(null, "r", 1);
+  container.appendChild(circle);
+}
