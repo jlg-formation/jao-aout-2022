51ad6f790d3c1e86592b3269354b5e7d298df4a4
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-08-19 16:17:39 +0200

    dev: test querySelector

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -1,4 +1,4 @@
-import { keys } from "../src/utils";
+import { keys, querySelector } from "../src/utils";
 
 test("adds 1 + 2 to equal 3", () => {
   expect(1 + 2).toBe(3);
@@ -6,4 +6,15 @@ test("adds 1 + 2 to equal 3", () => {
 
 test("keys returns Object.keys()", () => {
   expect(keys({ toto: 123, titi: false })).toStrictEqual(["toto", "titi"]);
+});
+
+test("querySelector throw an error", () => {
+  let foundError = false;
+  try {
+    const elt = querySelector("asdfasfd");
+    console.log("elt: ", elt);
+  } catch (err) {
+    foundError = true;
+  }
+  expect(foundError).toStrictEqual(true);
 });
